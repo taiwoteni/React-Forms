97a157faac35f6d2d4982aa5244572c45b0d766a
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-08-15 03:14:52 -0700

    Initil commit

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <ImageBackground
+  source={require('./assets/plants.jpg')}
+  style={styles.background}
+  >
+  <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+  </ImageBackground>
+
   );
 }
 
 const styles = StyleSheet.create({
+background:{
+flex: 1,
+     resizeMode: 'cover', // or 'contain'
+     justifyContent: 'center'
+},
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
